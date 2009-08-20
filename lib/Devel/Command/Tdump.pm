3dcbf84b1730fec90ef1a02dab12423e87cbd5d4
Joe McMahon <joe.mcmahon@gmail.com>
2009-08-20 15:31:55 -0700

    version 1.0  (released as Devel-Command-Tdump-1.tar.gz)

--- lib/Devel/Command/Tdump.pm
+++ lib/Devel/Command/Tdump.pm
@@ -1,169 +1,147 @@
 package Devel::Command::Tdump;
-
-use version; $VERSION = qv('0.0.1');
-
-use warnings;
 use strict;
-use Carp;
+use base qw(Devel::Command);
 
-# Other recommended modules (uncomment to use):
-#  use IO::Prompt;
-#  use Perl6::Export;
-#  use Perl6::Slurp;
-#  use Perl6::Say;
-#  use Regexp::Autoflags;
+$Devel::Command::Tdump::VERSION = 1.0;
+my %test_names = map { $_ => 1 } get_test_names();
+
+# Preload Test::More
+sub afterinit {
+  push @DB::typeahead, "use Test::More qw(no_plan)";
+}
+
+# Dump just the tests
+sub command {
+  my ($cmd) = @_;
+  my @tests; 
+  my $tfh;
+
+  my(undef, $outfile) = split(/\s+/,$cmd);
+  $outfile = "unnamed_test.t" unless defined $outfile;;
+
+  my $first_test = $DB::_first_test || 0;
+
+  print DB::OUT "Recording tests for this session in $outfile ...";
+  unless (open $tfh, ">$outfile") {
+     print DB::OUT " can't write history: $!\n";
+  }
+  else {
+    my @output;
+    my $test_count = 0;
+    my @lines = @DB::hist;
+
+    while (@lines) {
+      my $line = shift @lines;
+      my $forced_capture = 0;
+      # Following lines are comments?
+      if (@lines) { 
+        while ($lines[0] =~ /^\s*#/) {
+          # Yes. Print and discard.
+          push @output, $lines[0],"\n";
+          $forced_capture = 1;
+          shift @lines;
+        }
+      }
+      # skip this one unless we are supposed to keep it
+      # or it's a test
+      my $is_test;
+      next unless $forced_capture or 
+        ($is_test = is_a_test($line, \%test_names)); 
+      $test_count++ if $is_test;
+      $line = "$line;" unless $line =~ /;$/;
+      push @output, "$line\n";
+    }
+    unshift @output, "use Test::More tests=>$test_count;\n";
+    print $tfh @output;
+    close $tfh;
+    my $s = ($test_count == 1 ? "" : "s");
+    print DB::OUT " done ($test_count test$s).\n";
+    $DB::_first_test = $#DB::hist;
+  }
+}
+
+# Get the names defined in Test::More that are the names of tests
+# and save them in a debugger global.
+sub get_test_names {
+  my @names = keys %Test::More::;
+  grep { is_a_sub($_) } @names;
+}
 
 
-# Module implementation here
+# Returns true if this is a sub in Test::More, false otherwise
+sub is_a_sub {
+  local $_ = shift;
+  (!/^_/) and eval "defined &Test::More::$_";
+}
 
+# Returns true if this line of history is a Test::More test.
+sub is_a_test {
+  local $_    = shift;
+  my    $map  = shift;
+  if (my($possible, $paren) = /^\s*(\w+)\(/) {
+    return $map->{$possible};
+  }
+}
 
-1; # Magic true value required at end of module
+1;
+
 __END__
+
+=cut
 
 =head1 NAME
 
-Devel::Command::Tdump - [One line description of module's purpose here]
-
-
-=head1 VERSION
-
-This document describes Devel::Command::Tdump version 0.0.1
-
+Devel::TestEmbed - extend the debugger with Test::More
 
 =head1 SYNOPSIS
 
-    use Devel::Command::Tdump;
+  # We assume that the supplied perldb.sample has been
+  # copied to the appropriate place.
+  $ perl -demo
+  Loading DB routines from perl5db.pl version 1.27
+  Editor support available.
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
+  Enter h or `h h' for help, or `man perldebug' for more help.
+
+  main::(-e:1):   mo
+  auto(-1)  DB<1> use Test::More qw(no_plan)
+
+    DB<2> use_ok("CGI");
+
+    DB<3> $obj = new CGI;
+
+    DB<4> # Keep 'new CGI' in our test
+
+    DB<5> isa_ok($obj, "CGI")
+  ok 2 - The object isa CGI
+
+    DB<6> tdump "our.t"
+  Recording tests for this session in our.t ... done (2 tests).
+
+    DB<7> q
+  1..2
+  $ cat our.t
+  use Test::More tests=>2;
+  use_ok("CGI");
+  # Keep 'new CGI' in our test
+  $obj = new CGI;
+  isa_ok($obj, "CGI");
+
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+The C<Devel::TestEmbed> module loads C<Test::More> for you, allowing you to
+use its functions to test code; you may then save the tests you used in this
+debugger session via the C<tdump> function. 
 
+If needed, you may save "setup" code in the test as well by entering a 
+comment at the debugger prompt after each line of such code.
 
-=head1 INTERFACE 
+The module defines an C<afterinit> and C<watchfunction>; you will need to take this into account if you wish to defined either of these yourself while using this function. See C<perldoc perl5db.pl> for more informaton on these routines.
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=head1 BUGS
 
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-Devel::Command::Tdump requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-devel-command-tdump@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
+Package switching is not captured at the moment.
 
 =head1 AUTHOR
 
-Joe McMahon  C<< <mcmahon@cpan.org> >>
-
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2005, Joe McMahon C<< <mcmahon@cpan.org> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
-
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+Joe McMahon F<E<lt>mcmahon@perl.comE<gt>>.
